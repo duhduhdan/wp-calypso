7cc17cf892efc61b5e38904de8142859ecf7c177
Drew Blaisdell <drew.blaisdell@gmail.com>
2015-12-16 14:16:35 -0800

    Plans: Use functional stateless component syntax for `PlanUpgrade`

--- client/my-sites/plans/plan-overview/plan-upgrade/index.jsx
+++ client/my-sites/plans/plan-overview/plan-upgrade/index.jsx
@@ -7,40 +7,41 @@ import page from 'page';
 /**
  * Internal dependencies
  */
+import i18n from 'lib/mixins/i18n';
 import { cartItems } from 'lib/cart-values';
 import CompactCard from 'components/card/compact';
 import { isPremium } from 'lib/products-values';
 import * as upgradesActions from 'lib/upgrades/actions';
 
-const PlanUpgrade = React.createClass( {
-	propTypes: {
-		plan: React.PropTypes.object.isRequired,
-		selectedSite: React.PropTypes.oneOfType( [
-			React.PropTypes.object,
-			React.PropTypes.bool
-		] ).isRequired
-	},
-
-	upgradePlan( event ) {
+const PlanUpgrade = ( { plan, selectedSite: { slug: selectedSiteSlug } } ) => {
+	const upgradePlan = ( event ) => {
 		event.preventDefault();
 
 		upgradesActions.addItem( cartItems.planItem( 'business-bundle', true ) );
 
-		page( `/checkout/${ this.props.selectedSite.slug }` );
-	},
+		page( `/checkout/${ selectedSiteSlug }` );
+	};
 
-	render() {
-		if ( isPremium( this.props.plan ) ) {
-			return (
-				<CompactCard className="plan-upgrade">
-					{ this.translate( 'Need more power for your site? {{a}}Switch to the WordPress.com Business Free Trial.{{/a}}', {
-						components: { a: <a href="#" onClick={ this.upgradePlan } /> }
-					} ) }
-				</CompactCard>
-			);
-		}
-		return null;
+	if ( isPremium( plan ) ) {
+		return (
+			<CompactCard className="plan-upgrade">
+				{ i18n.translate( 'Need more power for your site? {{a}}Switch to the WordPress.com Business Free Trial.{{/a}}', {
+					components: { a: <a href="#" onClick={ upgradePlan } /> }
+				} ) }
+			</CompactCard>
+		);
 	}
-} );
+
+	// stateless functional components must return a ReactElement
+	return <noscript />;
+};
+
+PlanUpgrade.propTypes = {
+	plan: React.PropTypes.object.isRequired,
+	selectedSite: React.PropTypes.oneOfType( [
+		React.PropTypes.object,
+		React.PropTypes.bool
+	] ).isRequired
+};
 
 export default PlanUpgrade;
